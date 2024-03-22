67c57e4c8f93bd235f39137629a5460525f1e8fd
Rimas <dmitrij.karpenko.83@inbox.ru>
2024-03-22 10:02:26 +0500

    2 запроса и проверка подключения

--- server/index.js
+++ server/index.js
@@ -1,51 +1,112 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const app = express();
-app.use(express.json());
-
-const dbConfig = {
+const pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: '',
   database: 'timetable',
-};
-
-const pool = mysql.createPool(dbConfig);
-app.post('/register', (req, res) => {
-  const { name, password } = req.body;
-
-  if (!name || !password) {
-    return res.status(400).json({ error: 'Invalid data' });
-  }
-
-  const hashedPassword = hashPassword(password);
-  const query = 'INSERT INTO users (name, password) VALUES (?, ?)';
-  const values = [name, hashedPassword];
-
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error occurred during database connection:', err);
-      return res.status(500).json({ error: 'Database connection error' });
-    }
-
-    connection.query(query, values, (error, results) => {
-      connection.release();
-
-      if (error) {
-        console.error('Error occurred during registration:', error);
-        return res.status(500).json({ error: 'Registration failed' });
-      }
-
-      return res.status(200).json({ message: 'Registration successful' });
-    });
-  });
+  password: '',
 });
 
-function hashPassword(password) {
-  return password;
+// Обработчик POST-запроса на /register
+app.post('/register', (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    return res.status(416).json({ error: 'Данные не соответствуют запросу' });
+  }
+  if (userExists(username)) {
+    return res.status(409).json({ error: 'Имя пользователя уже существует' });
+  }
+  const newUser = {
+    username,
+    email,
+    password,
+  };
+  saveUser(newUser)
+    .then(() => {
+      res.status(200).json(newUser);
+    })
+    .catch((error) => {
+      console.error('Ошибка сохранения пользователя:', error);
+      res.status(500).json({ error: 'Ошибка сервера' });
+    });
+});
+
+//сохранение пользователя в бд
+function saveUser(user) {
+  return new Promise((resolve, reject) => {
+    const query = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+    const values = [user.username, user.email, user.password];
+
+    pool.query(query, values, (error, results) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
 }
 
+// Обработчик POST-запроса на /auth
+app.post('/auth', (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(416).json({ error: 'Данные не соответствуют запросу' });
+  }
+  authenticateUser(email, password)
+    .then((authenticated) => {
+      if (authenticated) {
+        res.status(200).json({ message: 'Пользователь успешно авторизован' });
+      } else {
+        res.status(401).json({ error: 'Не авторизован' });
+      }
+    })
+    .catch((error) => {
+      console.error('Ошибка проверки авторизации:', error);
+      res.status(500).json({ error: 'Ошибка сервера' });
+    });
+});
+
+// Функция проверки авторизации пользователя в бд
+function authenticateUser(email, password) {
+  return new Promise((resolve, reject) => {
+    const query = 'SELECT * FROM users WHERE email = ? AND password = ?';
+    const values = [email, password];
+
+    pool.query(query, values, (error, results) => {
+      if (error) {
+        reject(error);
+      } else {
+        const authenticated = results.length > 0;
+        resolve(authenticated);
+      }
+    });
+  });
+}
+
+
+
+
+
+// Запуск сервера
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Сервер запущен на порту 3000');
+  pool.getConnection((error, connection) => {
+    if (error) {
+      console.error('Ошибка подключения к базе данных:', error);
+      return;
+    }
+    console.log('Соединение с базой данных успешно установлено.');
+    connection.query('SELECT 1 + 1 AS result', (error, results) => {
+      if (error) {
+        console.error('Ошибка выполнения запроса:', error);
+        return;
+      }
+      console.log('Результат запроса:', results[0].result);
+      connection.release(); // Освобождение соединения обратно в пул
+      console.log('Соединение с базой данных успешно закрыто.');
+    });
+  });
 });
